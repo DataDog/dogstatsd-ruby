6975a8f75db24c22fca9571d9c714225b64d2033
Adrien Vaschalde <adrien@datadoghq.com>
2014-02-27 20:34:28 +0000

    Lots of random tests with Faker
    Had to add faker to Gemfile and update he helper
    Refs: #5

--- spec/statsd_spec.rb
+++ spec/statsd_spec.rb
@@ -340,60 +340,70 @@ describe Statsd do
     end
   end
   describe "#event" do
-    title = 'Title_test'
-    text = 'Text_test'
-    title_len = title.length
-    text_len = text.length
+    nb_tests = 100
+    for i in 00..nb_tests
+      title = Faker::Lorem.sentence(word_count = 10 * rand(3))
+      text = Faker::Lorem.paragraph(sentence_count = 100 * rand(5))
+      title_len = title.length
+      text_len = text.length
+      nb_tags = 10 * rand(2)
+      tags = Array.new
+      for j in 0..nb_tags
+        tag = String(Faker::Lorem.words(num = 10 * rand(10)))
+        tags.push(tag)
+      end
+      tags_joined = tags.join(",")
 
-    it "Only title and text" do
-      @statsd.event(title, text)
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}"]
-    end
-    it "With line break in Text and title" do
-      title_break_line = "#{title} \n second line"
-      text_break_line = "#{text} \n second line"
-      @statsd.event(title_break_line, text_break_line)
-      @statsd.socket.recv.must_equal ["_e{#{title_break_line.length},#{text_break_line.length}}:#{title_break_line}|#{text_break_line}"]
-    end
-    it "Event data string too long > 8KB" do
-      long_text = "#{text} " * 2000
-      proc {@statsd.event(title, long_text)}.must_raise RuntimeError
-    end
-    it "With known alert_type" do
-      @statsd.event(title, text, :alert_type => 'warning')
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|t:warning"]
-    end
-    it "With unknown alert_type" do
-      @statsd.event(title, text, :alert_type => 'bizarre')
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|t:bizarre"]
-    end
-    it "With known priority" do
-      @statsd.event(title, text, :priority => 'low')
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|p:low"]
-    end
-    it "With unknown priority" do
-      @statsd.event(title, text, :priority => 'bizarre')
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|p:bizarre"]
-    end
-    it "With hostname" do
-      @statsd.event(title, text, :hostname => 'hostname_test')
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|h:hostname_test"]
-    end
-    it "With aggregation_key" do
-      @statsd.event(title, text, :aggregation_key => 'aggkey 1')
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|k:aggkey 1"]
-    end
-    it "With source_type_name" do
-      @statsd.event(title, text, :source_type_name => 'source 1')
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|s:source 1"]
-    end
-    it "With several tags" do
-      @statsd.event(title, text, :tags => %w(tag_test_1 tag_test_2))
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|#tag_test_1,tag_test_2"]
-    end
-    it "With alert_type, priority, hostname, several tags" do
-      @statsd.event(title, text, :alert_type => 'warning', :priority => 'low', :hostname => 'hostname_test', :tags => %w(tag_test_1 tag_test_2))
-      @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|h:hostname_test|p:low|t:warning|#tag_test_1,tag_test_2"]
+      it "Only title and text" do
+        @statsd.event(title, text)
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}"]
+      end
+      it "With line break in Text and title" do
+        title_break_line = "#{title} \n second line"
+        text_break_line = "#{text} \n second line"
+        @statsd.event(title_break_line, text_break_line)
+        @statsd.socket.recv.must_equal ["_e{#{title_break_line.length},#{text_break_line.length}}:#{title_break_line}|#{text_break_line}"]
+      end
+      it "Event data string too long > 8KB" do
+        long_text = "#{text} " * 2000
+        proc {@statsd.event(title, long_text)}.must_raise RuntimeError
+      end
+      it "With known alert_type" do
+        @statsd.event(title, text, :alert_type => 'warning')
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|t:warning"]
+      end
+      it "With unknown alert_type" do
+        @statsd.event(title, text, :alert_type => 'bizarre')
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|t:bizarre"]
+      end
+      it "With known priority" do
+        @statsd.event(title, text, :priority => 'low')
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|p:low"]
+      end
+      it "With unknown priority" do
+        @statsd.event(title, text, :priority => 'bizarre')
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|p:bizarre"]
+      end
+      it "With hostname" do
+        @statsd.event(title, text, :hostname => 'hostname_test')
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|h:hostname_test"]
+      end
+      it "With aggregation_key" do
+        @statsd.event(title, text, :aggregation_key => 'aggkey 1')
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|k:aggkey 1"]
+      end
+      it "With source_type_name" do
+        @statsd.event(title, text, :source_type_name => 'source 1')
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|s:source 1"]
+      end
+      it "With several tags" do
+        @statsd.event(title, text, :tags => tags)
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|##{tags_joined}"]
+      end
+      it "With alert_type, priority, hostname, several tags" do
+        @statsd.event(title, text, :alert_type => 'warning', :priority => 'low', :hostname => 'hostname_test', :tags => tags)
+        @statsd.socket.recv.must_equal ["_e{#{title_len},#{text_len}}:#{title}|#{text}|h:hostname_test|p:low|t:warning|##{tags_joined}"]
+      end
     end
   end
 end
