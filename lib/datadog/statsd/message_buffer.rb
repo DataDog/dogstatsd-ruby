6979cc8f0c5e475e482d20ff7d22c855c8ba090a
Karim Bogtob <karim.bogtob@datadoghq.com>
2020-04-21 17:45:43 +0200

    Add strategy to either drop or raise when msg too long

--- lib/datadog/statsd/message_buffer.rb
+++ lib/datadog/statsd/message_buffer.rb
@@ -3,10 +3,15 @@
 module Datadog
   class Statsd
     class MessageBuffer
-      def initialize(connection, max_buffer_payload_size:, max_buffer_pool_size:)
+      def initialize(connection,
+        max_buffer_payload_size:,
+        max_buffer_pool_size:,
+        buffer_overflowing_stategy: :drop
+      )
         @connection = connection
         @max_buffer_payload_size = max_buffer_payload_size
         @max_buffer_pool_size = max_buffer_pool_size
+        @buffer_overflowing_stategy = buffer_overflowing_stategy
 
         @buffer = String.new
         @message_count = 0
@@ -32,6 +37,8 @@ module Datadog
         message_size = message.bytesize
         inner_message_count = message.count("\n") + 1
 
+        return nil unless ensure_sendable!(message_size)
+
         unless buffer.empty?
           if should_flush?(message_size, inner_message_count)
             flush
@@ -42,6 +49,7 @@ module Datadog
 
         buffer << message
         @message_count += inner_message_count
+        true
       end
 
       def flush
@@ -57,12 +65,24 @@ module Datadog
       attr :max_buffer_payload_size
       attr :max_buffer_pool_size
 
+      attr :buffer_overflowing_stategy
+
       attr :connection
       attr :buffer
 
       def should_flush?(message_size, inner_message_count)
         return true if buffer.bytesize + 1 + message_size >= max_buffer_payload_size
         return true if @message_count + inner_message_count >= max_buffer_pool_size
+
+        false
+      end
+
+      def ensure_sendable!(message_size)
+        return true if message_size <= max_buffer_payload_size
+
+        if buffer_overflowing_stategy == :raise
+          raise Error, 'Message too big for payload limit'
+        end
 
         false
       end
