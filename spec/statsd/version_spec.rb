0bd853ed83aff6a06073aaa81534fe5c6eafac66
Remy Mathieu <remy.mathieu@datadoghq.com>
2021-03-23 10:46:25 +0100

    Release 4.9.0

--- spec/statsd/version_spec.rb
+++ spec/statsd/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Datadog::Statsd do
   describe 'VERSION' do
     it 'has a version' do
-      expect(Datadog::Statsd::VERSION).to eq '4.8.3'
+      expect(Datadog::Statsd::VERSION).to eq '4.9.0'
     end
   end
 end
