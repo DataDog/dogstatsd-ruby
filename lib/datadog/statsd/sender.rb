f700f711a8a4bf9ed0ab40732461a8013f41a813
raymond zhao <raymond.zhao@datadoghq.com>
2025-02-24 10:15:02 -0500

    Log error message

--- lib/datadog/statsd/sender.rb
+++ lib/datadog/statsd/sender.rb
@@ -100,8 +100,8 @@ module Datadog
           # start background thread
           @sender_thread = @thread_class.new(&method(:send_loop))
           @sender_thread.name = "Statsd Sender" unless Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.3')
-        rescue ThreadError
-          @logger.debug { "Statsd: Failed to start sender thread, flushing message buffer" } if @logger
+        rescue ThreadError => e
+          @logger.debug { "Statsd: Failed to start sender thread: #{e.message}, flushing message buffer" } if @logger
           message_buffer.flush
         end
 
